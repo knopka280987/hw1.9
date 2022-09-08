cb263441504d41d23c7b038f323f9c61a65a05a7
Татьяна <fokT.M@yandex.ru>
2022-09-08 14:07:10 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       var author = new Author("Ivan", "Ivanov");
+       var author1 = new Author("Ivan1", "Ivanov1");
+       var author2 = new Author("Ivan2", "Ivanov2");
+       var book1 = new Book("Book1", author, 1951);
+       var book2 = new Book("Book2", author1, 1952);
+       var book3 = new Book("Book3", author2, 1953);
+
+        System.out.println(author);
+        System.out.println(book1);
     }
 }
